3c11bf87de1001de50d0f6acc5f0d5cf59a8561f
yyjun1649 <yy164946@naver.com>
2023-02-08 11:33:05 +0900

    Graph 기본

---
+++ GraphBasic.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// 그래프 기본
+// 그래프 생성 및 탐색 3가지 방법
+
+
+void CreateGraph_1()
+{
+	struct Vertex
+	{
+		vector<Vertex*> edges;
+		// int data;
+	};
+
+	vector<Vertex> v;
+	v.resize(6);
+
+	v[0].edges.push_back(&v[1]);
+	v[0].edges.push_back(&v[3]);
+	v[1].edges.push_back(&v[0]);
+	v[1].edges.push_back(&v[2]);
+	v[1].edges.push_back(&v[3]);
+	v[3].edges.push_back(&v[4]);
+	v[5].edges.push_back(&v[4]);
+
+	// Find
+	bool connected = false;
+	for (Vertex* edge : v[0].edges)
+	{
+		if (edge == &v[3])
+		{
+			connected = true;
+			break;
+		}
+	}
+}
+
+// 정점이 100개
+// - 지하철 노선도 -> 서로 드문 드문 연결 (양옆, 환승역이라면 조금 더 ++)
+void CreateGraph_2()
+{
+	struct Vertex
+	{
+		// int data;
+	};
+
+	vector<Vertex> v;
+	v.resize(6);
+
+	// 연결된 목록을 따로 관리
+	// adjacent[n] -> n번째 정점과 연결된 정점 목록
+	vector<vector<int>> adjacent(6);
+	adjacent[0] = { 1, 3 };
+	adjacent[1] = { 0, 2, 3 };
+	adjacent[3] = { 4 };
+	adjacent[5] = { 4 };
+
+	// Find
+	bool connected = false;
+	for (int vertex : adjacent[0])
+	{
+		if (vertex == 3)
+		{
+			connected = true;
+			break;
+		}
+	}
+
+	// STL
+	vector<int>& adj = adjacent[0];
+	bool connected2 = (std::find(adj.begin(), adj.end(), 3) != adj.end());
+}
+
+// 정점이 100개
+// - 페이스북 친구 -> 서로 빽빽하게 연결
+// 연결된 목록을 따로 관리
+// [X][O][X][O][X][X]
+// [O][X][O][O][X][X]
+// [X][X][X][X][X][X]
+// [X][X][X][X][O][X]
+// [X][X][X][X][X][X]
+// [X][X][X][X][O][X]
+void CreateGraph_3()
+{
+	struct Vertex
+	{
+		// int data;
+	};
+
+	vector<Vertex> v;
+	v.resize(6);
+
+	// 읽는 방법 : adjacent[from][to]
+	// 행렬을 이용한 그래프 표현 (2차원 배열)
+	// 메모리 소모가 심하지만, 빠른 접근이 가능하다
+	// (간선이 많은 경우 이점이 있다)
+	vector<vector<bool>> adjacent(6, vector<bool>(6, false));
+	adjacent[0][1] = true;
+	adjacent[0][3] = true;
+	adjacent[1][0] = true;
+	adjacent[1][2] = true;
+	adjacent[1][3] = true;
+	adjacent[3][4] = true;
+	adjacent[5][4] = true;
+
+	// Find
+	bool connected = adjacent[0][3];
+
+	// 간선의 값이 존재할 때
+	vector<vector<int>> adjacent2 =
+	{
+		vector<int> { -1, 15, -1, 35, -1, -1 },
+		vector<int> { 15, -1,  5, 10, -1, -1 },
+		vector<int> { -1, -1, -1, -1, -1, -1 },
+		vector<int> { -1, -1, -1, -1,  5, -1 },
+		vector<int> { -1, -1, -1, -1, -1, -1 },
+		vector<int> { -1, -1, -1, -1,  5, -1 },
+	};
+}
