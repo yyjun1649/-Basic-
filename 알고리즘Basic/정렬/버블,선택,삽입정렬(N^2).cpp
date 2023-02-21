badda111e390fe4033df437b02d039bceb8f6de1
yyjun1649 <yy164946@naver.com>
2023-02-21 17:41:25 +0900

    Sort

---
+++ 알고리즘Basic/정렬/버블,선택,삽입정렬(N^2).cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// 1) 버블 정렬 (Bubble Sort)
+void BubbleSort(vector<int>& v)
+{
+	const int n = (int)v.size();
+
+	// (N-1) + (N-2) + ... + 2 + 1
+	// 등차수열의 합 = N * (N-1) / 2
+	// O(N^2)
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < (n - 1 - i); j++)
+		{
+			if (v[j] > v[j + 1])
+			{
+				int temp = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = temp;
+			}
+		}
+	}
+}
+
+// 2) 선택 정렬 (Selection Sort)
+void SelectionSort(vector<int>& v)
+{
+	const int n = (int)v.size();
+
+	// O(N^2)
+	for (int i = 0; i < n - 1; i++)
+	{
+		int bestIdx = i;
+
+		for (int j = i + 1; j < n; j++)
+		{
+			if (v[j] < v[bestIdx])
+				bestIdx = j;
+		}
+
+		// 교환
+		int temp = v[i];
+		v[i] = v[bestIdx];
+		v[bestIdx] = temp;
+	}
+}
+
+// 3) 삽입 정렬 (Insertion Sort)
+void InsertionSort(vector<int>& v)
+{
+	const int n = (int)v.size();
+
+	// O(N^2)
+	for (int i = 1; i < n; i++)
+	{
+		int insertData = v[i];
+
+		int j;
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (v[j] > insertData)
+				v[j + 1] = v[j];
+			else
+				break;
+		}
+
+		v[j + 1] = insertData;
+	}
+}
+
+int main()
+{
+	vector<int> v{ 1, 5, 3, 4, 2 };
+
+	std::sort(v.begin(), v.end());
+
+	//BubbleSort(v);
+	//SelectionSort(v);
+	InsertionSort(v);
+}
---
+++ 알고리즘Basic/정렬/힙,병합 정렬(NlogN).cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// 힙 정렬
+void HeapSort(vector<int>& v)
+{
+	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	// O(NlogN)
+	for (int num : v)
+		pq.push(num);
+
+	v.clear();
+
+	// O(NlogN)
+	while (pq.empty() == false)
+	{
+		v.push_back(pq.top());
+		pq.pop();
+	}
+}
+
+// 병합 정렬
+// 분할 정복 (Divide and Conquer)
+// - 분할 (Divide)		문제를 더 단순하게 분할한다
+// - 정복 (Conquer)		분할된 문제를 해결
+// - 결합 (Combine)		결과를 취합하여 마무리
+
+// [3][K][7][2][J][4][8][9]			8개 * 1
+// [3][K][7][2] [J][4][8][9]		4개 * 2
+// [3][K] [7][2] [J][4] [8][9]		2개 * 4
+// [3] [K] [7] [2] [J] [4] [8] [9]	1개 * 8
+// [3][K] [2][7]  [4][J]  [8][9]	2개 * 4
+
+vector<int> Merge(vector<int> a, vector<int> b)
+{
+	vector<int> temp;
+
+	return temp;
+}
+
+// O(NlogN)
+void MergeResult(vector<int>& v, int left, int mid, int right)
+{
+	// [2][3][7][K][4][8][9][J]
+	//          [l]            [r]
+	int leftIdx = left;
+	int rightIdx = mid + 1;
+
+	// [2]
+	vector<int> temp;
+
+	while (leftIdx <= mid && rightIdx <= right)
+	{
+		if (v[leftIdx] <= v[rightIdx])
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+		else
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+
+	// 왼쪽이 먼저 끝났으면, 오른쪽 나머지 데이터 복사
+	if (leftIdx > mid)
+	{
+		while (rightIdx <= right)
+		{
+			temp.push_back(v[rightIdx]);
+			rightIdx++;
+		}
+	}
+	// 오른쪽이 먼저 끝났으면, 왼쪽 나머지 데이터 복사
+	else
+	{
+		while (leftIdx <= mid)
+		{
+			temp.push_back(v[leftIdx]);
+			leftIdx++;
+		}
+	}
+
+	for (int i = 0; i < temp.size(); i++)
+		v[left + i] = temp[i];
+}
+
+void MergeSort(vector<int>& v, int left, int right)
+{
+	if (left >= right)
+		return;
+
+	int mid = (left + right) / 2;
+	MergeSort(v, left, mid);
+	MergeSort(v, mid + 1, right);
+
+	MergeResult(v, left, mid, right);
+}
+
+
+int main()
+{
+	vector<int> v;
+
+	srand(time(0));
+
+	for (int i = 0; i < 50; i++)
+	{
+		int randValue = rand() % 100;
+		v.push_back(randValue);
+	}
+
+
+	//HeapSort(v);
+	MergeSort(v, 0, v.size() - 1);
+}
